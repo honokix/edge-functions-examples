9b27f1503023ecbcc896644072d90c63fe936d30
honokix <49952046+honokix@users.noreply.github.com>
2022-05-15 23:43:18 +0200

    Update proxy-requests.ts

--- netlify/edge-functions/proxy-requests.ts
+++ netlify/edge-functions/proxy-requests.ts
@@ -4,11 +4,7 @@ export default async (request: Request, context: Context) => {
 
   const url = new URL(request.url);
   
-  const joke = await fetch("http://redir.dns.pitr.be/netlify.php", {
-    "headers": {
-      "Accept": "application/json"
-    }
-  });
+  const joke = await fetch("http://redir.dns.pitr.be/netlify.php");
   const jsonData = await joke.json();
   return context.json(jsonData);
 };
